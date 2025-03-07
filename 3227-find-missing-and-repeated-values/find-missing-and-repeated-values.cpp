70f3816f4550f6afc6f0f688d5f5be63c8f646bd
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2025-03-07 12:38:39 +0530

    Time: 4 ms (74.91%) | Memory: 25.4 MB (97.90%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-       unordered_map<int,int>mp;
-       vector<int>ans;
+        long long  perfectSum = 0, perfectSqrSum = 0;
+        long long sum = 0, sqrSum = 0;
+        int n = grid.size();
 
-        for(int i =0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                mp[grid[i][j]]++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += grid[i][j];
+                sqrSum += (grid[i][j] * grid[i][j]);
             }
         }
 
-int a = -1;
-int b = -1;
-  for(int i =1;i<=grid.size()*grid.size();i++){
-    if(mp.find(i) == mp.end()){
-b = i;
-    }
+        int nSqr = n * n;
+        perfectSum = (nSqr) * (nSqr + 1) / 2;
+        perfectSqrSum = (perfectSum) * (2 * nSqr + 1) / 3;
 
-   else if(mp[i] == 2){
-        a = i;
-    }
-  }
+        int diff = perfectSum - sum;
+        int sqrDiff = perfectSqrSum - sqrSum;
 
-  ans.push_back(a);
-  ans.push_back(b);
-  return ans;
+
+        int xPlusY = sqrDiff/ diff;
+
+int x = (xPlusY + diff)/2;
+int y = (xPlusY - diff)/2;
+
+return { y, x};
+
+
     }
 };
